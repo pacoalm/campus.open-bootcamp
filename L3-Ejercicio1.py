27186088197c2f5b905fff3a42263882fc1bf371
pacoalm <88049825+pacoalm@users.noreply.github.com>
2022-07-07 09:16:37 +0100

    Create L3-Ejercicio1.py

---
+++ L3-Ejercicio1.py
@@ -0,0 +1,21 @@
+Nombre = "Francisco"
+Apellidos= "Rodriguez Almeida"
+Edad = 55
+Email = "pacoalm@gmail.com"
+Telefono = "555-111-222"
+Casado = False
+ConHijos = True 
+Amigos=['Pedro','Victor','Javi','Manuel']
+Peliculas = {"p1":"Avatar","p2":"Star Wars", "p3":"Harry Potter"}
+
+print(Nombre)
+print(Apellidos)
+print(Edad)
+print(Email)
+print(Telefono)
+print(Casado)
+print(ConHijos)
+print(Amigos)
+print(Amigos[0])
+print (Peliculas)
+print(Peliculas["p3"]);
